237806fcab7f3c60027f5811c5ab0daf8d9ebf6b
MattMan569 <matthewpolsom@hotmail.com>
2021-09-03 20:53:46 -0600

    add angular material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]

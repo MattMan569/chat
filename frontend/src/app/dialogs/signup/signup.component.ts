18d78e924bf05dd3a5ab575526e4ed510cb84395
MattMan569 <matthewpolsom@hotmail.com>
2021-09-28 20:44:23 -0600

    create signup

--- frontend/src/app/dialogs/signup/signup.component.ts
+++ frontend/src/app/dialogs/signup/signup.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
+  @ViewChild('usernameInput') private usernameInput!: ElementRef;
+  @ViewChild('passwordInput') private passwordInput!: ElementRef;
+  @ViewChild('confirmInput') private confirmInput!: ElementRef;
 
-  constructor() { }
+  constructor(
+    private dialogRef: MatDialogRef<SignupComponent>,
+    private authService: AuthService,
+  ) { }
 
-  ngOnInit(): void {
+  async onSignup() {
+    const username = (this.usernameInput.nativeElement as HTMLInputElement).value;
+    const password = (this.passwordInput.nativeElement as HTMLInputElement).value;
+    const confirm = (this.confirmInput.nativeElement as HTMLInputElement).value;
+
+    // TODO
+    if (password !== confirm) {
+      console.log('mismatch');
+      return;
+    }
+
+    await this.authService.signup(username, password);
+    this.dialogRef.close();
   }
-
 }

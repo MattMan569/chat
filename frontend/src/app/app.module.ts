430d5a6d1633cabea5991fb5cf5b8daf81dd33f8
MattMan569 <matthewpolsom@hotmail.com>
2021-09-28 20:45:03 -0600

    create api calls

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AngularMaterialModule } from './angular-material.module';
 
@@ -21,7 +22,8 @@ import { ChatComponent } from './chat/chat.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,5 +1,9 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const SERVER_URL = `${environment.apiUrl}/user`;
 
 @Injectable({
   providedIn: 'root'
@@ -7,24 +11,45 @@ import { BehaviorSubject } from 'rxjs';
 export class AuthService {
   private authStatus = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getAuthStatus() {
     return this.authStatus.asObservable();
   }
 
-  signup() {
-    this.authStatus.next(true);
+  signup(username: string, password: string) {
+    return new Promise<string | void>((resolve) => {
+      this.http.post(`${SERVER_URL}/signup`, { username, password }, { withCredentials: true }).subscribe(() => {
+        resolve();
+        this.authStatus.next(true);
+      }, (error: HttpErrorResponse) => {
+        console.error(error);
+        resolve(error.name);
+      });
+    });
   }
 
   login(username: string, password: string) {
-    return new Promise<string>((resolve, reject) => {
-      resolve('nice');
-      this.authStatus.next(true);
+    return new Promise<string | void>((resolve) => {
+      this.http.post(`${SERVER_URL}/login`, { username, password }, { withCredentials: true }).subscribe(() => {
+        resolve();
+        this.authStatus.next(true);
+      }, (error: HttpErrorResponse) => {
+        console.error(error);
+        resolve(error.name);
+      });
     });
   }
 
   logout() {
-    this.authStatus.next(false);
+    return new Promise<string | void>((resolve) => {
+      this.http.post(`${SERVER_URL}/logout`, null, { withCredentials: true }).subscribe(() => {
+        resolve();
+        this.authStatus.next(false);
+      }, (error: HttpErrorResponse) => {
+        console.error(error);
+        resolve(error.name);
+      });
+    });
   }
 }
